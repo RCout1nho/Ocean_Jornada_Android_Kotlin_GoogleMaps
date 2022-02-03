20fe911ac0b13de09aa16d0c788359b2424bb13b
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-02-03 09:25:45 -0400

    Google Maps API added

---
+++ app/src/androidTest/java/com/example/ocean_jornada_android_kotlin_googlemaps/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.ocean_jornada_android_kotlin_googlemaps
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.ocean_jornada_android_kotlin_googlemaps", appContext.packageName)
+    }
+}
